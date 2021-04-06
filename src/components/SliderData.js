a4e544ab21adde52d5b28061d808ee2124829d46
Imon <contact@imonweb.co.uk>
2021-04-06 15:19:10 +0100

    slider images added

---
+++ src/components/SliderData.js
@@ -0,0 +1,15 @@
+export const SliderData = [
+  {
+    image: 'https://images.unsplash.com/photo-1615865317996-84d7ca797476?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=879&q=80'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1534227749049-06c3ed80963c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1525972024754-45abff399a29?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1567457399160-08d6ee75ac86?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
+  }
+];
+
